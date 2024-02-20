bff43c0ce159c4ac6241332c4fa62d112833d7a6
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-20 17:28:14 +0530

    navbar and links

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -1,20 +1,37 @@
-import "./navbar.scss"
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        {/* Sidebar */}
-        <div className="wrapper">
-            <span>Pankaj Dwivedi</span>
-            <div className="social">
-                <a href="#"><img src="/linkedin.png" alt="" height={64} width={64} /></a>
-                <a href="#"><img src="/github.png" alt=""  height={64} width={64} /></a>
-                <a href="#"><img src="/instagram.png" alt="" /></a>
-                <a href="#"><img src="/twitter.png" alt=""  height={64} width={64} /></a>
-            </div>
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Pankaj Dwivedi
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/linkedin.png" alt="" height={64} width={64} />
+          </a>
+          <a href="#">
+            <img src="/github.png" alt="" height={64} width={64} />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/twitter.png" alt="" height={64} width={64} />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
